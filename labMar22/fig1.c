7123f3bd50fb81e745b54c85e3355c82bfe9b5c6
Johan Ferreras <ferrerasjohan@gmail.com>
2018-03-23 14:11:43 -0400

    second commit

---
+++ labMar22/fig1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *child(void *arg) {
+  printf("child\n");
+  // XXX how to indicate we are done?
+  return NULL;
+}
+
+int main(int argc, char *argv[]){
+  printf("parent: begin\n");
+  pthread_t c;
+  pthread_create(&c, NULL, child, NULL); //create child
+  // XXX how to wait for child?
+  printf("parent: end\n");
+  return 0;
+}
---
+++ prog4/prog4swap.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PAGE_ACCESSES 10000
+#define PAGES 100
